354daad0ab64dcd7105a99d3fb9056ba7b787e3d
dashamet <metropolitansky@college.harvard.edu>
2019-11-17 01:15:35 -0500

    Updated data formatting, added dashed lines for missing data

--- js/vis-line.js
+++ js/vis-line.js
@@ -4,8 +4,10 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     height = 500 - margin.top - margin.bottom;
 
 // set the ranges
-var x = d3.scaleLinear().range([0, width]);
+var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
+
+var parseTime = d3.timeParse("%Y")
 
 // define the 1st line
 var valueline = d3.line()
@@ -22,6 +24,17 @@ var valueline3 = d3.line()
     .x(function(d) {return x(d.year); })
     .y(function(d) {return y(d.unintentional_deaths); })
 
+// define the 4th line
+var valueline4 = d3.line()
+    .defined(d => !isNaN(d.unintentional_injuries))
+    .x(d => x(d.year))
+    .y(d => y(d.unintentional_injuries))
+
+//define the 5th line
+var valueline5 = d3.line()
+    .defined(d => !isNaN(d.intentional_injuries))
+    .x(d => x(d.year))
+    .y(d => y(d.intentional_injuries))
 
 // appends the svg object to the body of the page
 // appends a 'group' element to 'svg'
@@ -36,25 +49,31 @@ var svg = d3.select("body").append("svg")
 // Get the data
 d3.csv("data/aggregated-deaths-injuries.csv", function(data) {
     // format the data
+
     data.forEach(function(d) {
+        d.year = parseTime(d.year);
         d.intentional_homicides = +d.intentional_homicides;
         d.intentional_suicides = +d.intentional_suicides;
         d.unintentional_deaths = +d.unintentional_deaths;
+        d.unintentional_injuries = Number(d.unintentional_injuries);
+        d.intentional_injuries = Number(d.intentional_injuries);
     });
     console.log(data)
 
     // Scale the range of the data
     x.domain(d3.extent(data, function(d) { return d.year; }));
     y.domain([0, d3.max(data, function(d) {
-        return Math.max(d.intentional_homicides, d.intentional_suicides, d.unintentional_deaths); })]);
+        return Math.max(d.intentional_homicides, d.intentional_suicides, d.unintentional_deaths, d.unintentional_injuries,
+            d.intentional_injuries); })]);
 
     // Add the valueline path.
 
     svg.append("path")
         .data([data])
         .attr("class", "line")
-        .style("stroke", "red")
+        .style("stroke", "green")
         .style("fill","none")
+        .attr("stroke-width", 2)
         .attr("d", valueline);
 
     // Add the valueline2 path.
@@ -63,15 +82,47 @@ d3.csv("data/aggregated-deaths-injuries.csv", function(data) {
         .attr("class", "line")
         .style("stroke", "red")
         .style("fill","none")
+        .attr("stroke-width", 1.5)
         .attr("d", valueline2)
 
     // Add the valueline3 path.
     svg.append("path")
         .data([data])
         .attr("class", "line")
-        .style("stroke", "red")
+        .style("stroke", "blue")
         .style("fill","none")
+        .attr("stroke-width", 1.5)
         .attr("d", valueline3);
+
+    // Add the valueline4 paths.
+    svg.append("path")
+        .datum(data.filter(valueline4.defined()))
+        .attr("stroke", "orange")
+        .style("stroke-dasharray", ("3, 3"))
+        .style("fill","none")
+        .attr("d", valueline4);
+
+    svg.append("path")
+        .datum(data)
+        .attr("stroke", "orange")
+        .style("fill","none")
+        .attr("stroke-width", 1.5)
+        .attr("d", valueline4);
+
+    // Add the valueline5 paths.
+    svg.append("path")
+        .datum(data.filter(valueline5.defined()))
+        .attr("stroke", "black")
+        .style("stroke-dasharray", ("3, 3"))
+        .style("fill","none")
+        .attr("d", valueline5);
+
+    svg.append("path")
+        .datum(data)
+        .attr("stroke", "black")
+        .style("fill","none")
+        .attr("stroke-width", 1.5)
+        .attr("d", valueline5);
 
     // Add the X Axis
     svg.append("g")
@@ -81,5 +132,6 @@ d3.csv("data/aggregated-deaths-injuries.csv", function(data) {
     // Add the Y Axis
     svg.append("g")
         .call(d3.axisLeft(y));
+
 
 });
