0787ef793ab1fc6de7d88c76eb42823302701a7c
dashamet <metropolitansky@college.harvard.edu>
2019-11-17 13:27:44 -0500

    Added points, tooltip, and pop-ups

--- js/vis-line.js
+++ js/vis-line.js
@@ -3,11 +3,15 @@ var margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-// set the ranges
+// Setting the ranges
 var x = d3.scaleTime().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 
+// Setting up a function that will covert a year string to a date
 var parseTime = d3.timeParse("%Y")
+
+// Setting up a colour scale
+var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 // define the 1st line
 var valueline = d3.line()
@@ -50,6 +54,8 @@ var svg = d3.select("body").append("svg")
 d3.csv("data/aggregated-deaths-injuries.csv", function(data) {
     // format the data
 
+    color.domain(d3.keys(data[0]).filter(function(key) { return key !== "year"; }));
+
     data.forEach(function(d) {
         d.year = parseTime(d.year);
         d.intentional_homicides = +d.intentional_homicides;
@@ -57,8 +63,19 @@ d3.csv("data/aggregated-deaths-injuries.csv", function(data) {
         d.unintentional_deaths = +d.unintentional_deaths;
         d.unintentional_injuries = Number(d.unintentional_injuries);
         d.intentional_injuries = Number(d.intentional_injuries);
+        mod_data = color.domain().map(function(name) {
+            return {
+                name: name,
+                values: data.map(function(d) {
+                    return {year: d.year, victims: d[name]};
+                })
+            };
+        });
+
+        console.log(mod_data)
     });
-    console.log(data)
+
+    //console.log(data)
 
     // Scale the range of the data
     x.domain(d3.extent(data, function(d) { return d.year; }));
@@ -67,7 +84,6 @@ d3.csv("data/aggregated-deaths-injuries.csv", function(data) {
             d.intentional_injuries); })]);
 
     // Add the valueline path.
-
     svg.append("path")
         .data([data])
         .attr("class", "line")
@@ -123,6 +139,56 @@ d3.csv("data/aggregated-deaths-injuries.csv", function(data) {
         .style("fill","none")
         .attr("stroke-width", 1.5)
         .attr("d", valueline5);
+
+    var records = svg.selectAll(".records")
+        .data(mod_data)
+        .enter().append("g")
+        .attr("class", "records");
+
+    var div = d3.select("body")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    records.append("g").selectAll("circle")
+        .data(function(d){return d.values})
+        .enter()
+        .append("circle")
+        .attr("r", function(d){
+            if(isNaN(d.victims)){
+                return 0
+            }
+            else{return 4}
+        })
+        .attr("cx", function(d){return x(d.year)})
+        .attr("cy", function(d){return y(d.victims)})
+        .attr("fill", "black")
+        .attr("stroke", "black")
+        .on('mouseover', function (d, i) {
+            d3.select(this).transition()
+                .duration('50')
+                .attr('opacity', '.85');
+            div.transition()
+                .duration(50)
+                .style("opacity", 1);
+            div.html(function () {
+                if (isNaN(d.victims)) {
+                    return "<strong>" + d.year.getFullYear() + "</strong>" + " <br> " + "No data available"
+                } else {
+                    return "<strong>" + d.year.getFullYear() + "</strong>" + " <br> " + d.victims
+                }
+            })
+                .style("left", (d3.event.pageX + 10) + "px")
+                .style("top", (d3.event.pageY - 15) + "px");
+        })
+        .on('mouseout', function (d, i) {
+            d3.select(this).transition()
+                .duration('50')
+                .attr('opacity', '1');
+            div.transition()
+                .duration('50')
+                .style("opacity", 0);
+        })
 
     // Add the X Axis
     svg.append("g")
