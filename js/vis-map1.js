1bbdffc94347b6ae6c3d7be2613403e944cee14f
Jessica Bishai <jess.bishai@gmail.com>
2019-11-17 20:37:11 -0500

    starting on maps

--- js/vis-map1.js
+++ js/vis-map1.js
@@ -3,13 +3,13 @@ var margin = {top: 30, right: 180, bottom: 30, left: 80},
     width = 1200 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var svg = d3.select("#map1").append("svg")
+var svgM1 = d3.select("#map1").append("svg")
     .attr("width", width)
     .attr("height", height);
 
 var projection = d3.geoMercator()
     .translate([width / 2, height / 2])
-    .scale([100]);
+    .scale([1000]);
 
 var path = d3.geoPath()
     .projection(projection);
@@ -18,7 +18,7 @@ var path = d3.geoPath()
 d3.json("data/us.topo.json", function(data) {
     console.log(data);
     var us = topojson.feature(data, data.objects.state).features;
-    svg.selectAll("path")
+    svgM1.selectAll("path")
         .data(us)
         .enter().append("path")
         .attr("d", path);
