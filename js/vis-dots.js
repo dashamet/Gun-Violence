b63972c81bfd82d40f248f15af60966c2f9e6edc
Jessica Bishai <jess.bishai@gmail.com>
2019-11-20 16:42:14 -0500

    js file for dot visualization

---
+++ js/vis-dots.js
@@ -0,0 +1,7 @@
+var margin = {top: 30, right: 180, bottom: 30, left: 80},
+    width = 1200 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var svg = d3.select("#memorial-viz").append("svg")
+    .attr("width", width)
+    .attr("height", height);
