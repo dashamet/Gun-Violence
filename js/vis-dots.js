23fa2faf497db5f4659c598e33b14be075b7ec66
Jessica Bishai <jess.bishai@gmail.com>
2019-11-21 23:41:31 -0500

    working on dot visualization

--- js/vis-dots.js
+++ js/vis-dots.js
@@ -25,15 +25,6 @@ d3.csv("data/allShootings.csv", function(data) {
         })
         .attr('cx', function(d,i){
             return width/50*(3.5+i%40)
-            // if(i % 40 === 0){
-            //     return width/10*3.5;
-            // } else if(i % 40 === 1) {
-            //     return width/10 * 4.5
-            // } else if(i % 40 === 2) {
-            //     return width/10 * 5.5
-            // } else if(i % 4 === 3) {
-            //     return width/10 * 6.5
-            // }
         })
         .attr('cy', function(d,i){
             return (Math.trunc(i / 40)) * height/50 + marginTop;
@@ -46,4 +37,153 @@ d3.csv("data/allShootings.csv", function(data) {
         //         return 'red'
         //     }
         // });
+    // animation 1 - arrange circles as two circles
+
+    function animateGender(){
+        d3.selectAll('.customCircle.male')
+            .transition()
+            .duration(800)
+            .attr('r' radiusSmallCircles)
+        d3.selectAll('.customCircle.female')
+        d3.selectAll('.customCircle.Undefined')
+    }
+
+    function animationOne(){
+
+        // update circles
+        d3.selectAll('.customCircle.child')
+            .transition()
+            .duration(800)
+            .attr('r', radiusSmallCircles)
+            .attr('cx', function(d,i){
+                let angle = (i / (data.length/4)) * Math.PI;
+                let x = (radiusBigCircles * Math.cos(angle)) + (width/4);
+                return x })
+            .attr('cy', function(d,i){
+                let angle = (i / (data.length/4)) * Math.PI;
+                return (radiusBigCircles * Math.sin(angle)) + (height/2);
+            });
+
+        d3.selectAll('.customCircle.teen')
+            .transition()
+            .duration(800)
+            .attr('r', radiusSmallCircles)
+            .attr('cx', function(d,i){
+                let angle = (i / (data.length/4)) * Math.PI;
+                let x = (radiusBigCircles * Math.cos(angle)) + (width/4*3);
+                return x })
+            .attr('cy', function(d,i){
+                let angle = (i / (data.length/4)) * Math.PI;
+                return (radiusBigCircles * Math.sin(angle)) + (height/2);
+            });
+
+        // update labels
+        d3.selectAll('.customLabel.child')
+            .transition()
+            .duration(800)
+            .attr('r', radiusSmallCircles)
+            .attr('x', function(d,i){
+                let angle = (i / (data.length/4)) * Math.PI;
+                let x = (radiusBigCircles * Math.cos(angle)) + (width/4);
+                return x })
+            .attr('y', function(d,i){
+                let angle = (i / (data.length/4)) * Math.PI;
+                y = (radiusBigCircles * Math.sin(angle)) + (height/2);
+                return y
+            });
+
+        // update labels
+        d3.selectAll('.customLabel.adult')
+            .transition()
+            .duration(800)
+            .attr('r', radiusSmallCircles)
+            .attr('x', function(d,i){
+                let angle = (i / (data.length/4)) * Math.PI;
+                return (radiusBigCircles * Math.cos(angle)) + (width/4*3);
+            })
+            .attr('y', function(d,i){
+                let angle = (i / (data.length/4)) * Math.PI;
+                return (radiusBigCircles * Math.sin(angle)) + (height/2);
+            });
+    }
+
+
+
+// animation 2 - separate bar charts
+    function animationTwo(){
+
+        d3.selectAll('.customCircle.child')
+            .transition()
+            .duration(800)
+            .attr('cx', function(d,i){
+                return (width/100*(i%4+1))
+            })
+            .attr('cy', function(d,i){
+                return (Math.trunc(i / 4)) * (radiusAnimationTwo*2 + spaceBetweenCircles);
+            })
+            .attr('r', radiusAnimationTwo);
+
+
+        d3.selectAll('.customCircle.teen')
+            .transition()
+            .duration(800)
+            .attr('cx', function(d,i){
+                return (width/100*(6+i%4))
+            })
+            .attr('cy', function(d,i){
+                return (Math.trunc(i / 4)) * (radiusAnimationTwo*2 + spaceBetweenCircles);
+            })
+            .attr('r', radiusAnimationTwo);
+
+
+        // update labels
+        d3.selectAll('.customLabel.child')
+            .transition()
+            .duration(800)
+            .attr('x', function(d,i){
+                return width/100*(1+i%4)
+            })
+            .attr('y', function(d,i){
+                return (Math.trunc(i / 4)) * (radiusAnimationTwo*2 + spaceBetweenCircles);
+            })
+            .attr('r', radiusAnimationTwo);
+
+
+        // update labels
+        d3.selectAll('.customLabel.adult')
+            .transition()
+            .duration(800)
+            .attr('x', function(d,i){
+                return width/100*(i%4+6)
+            })
+            .attr('y', function(d,i){
+                return (Math.trunc(i / 4)) * (radiusAnimationTwo*2 + spaceBetweenCircles);
+            })
+            .attr('r', radiusAnimationTwo);
+    }
+
+// animation 3 - back to start
+    function animationThree(){
+
+        // sort by
+        d3.selectAll('.customCircle')
+            .transition()
+            .duration(800)
+            .attr('cx', function(d,i){
+                return width/50*(3.5+i%40)
+            })
+            .attr('cy', function(d,i){
+                return (Math.trunc(i / 40)) * height/50 + marginTop;
+            })
+            .attr('r', width/300);
+    }
+
+// animation
+    setInterval (animateCircles, 6000);
+
+    function animateCircles(){
+        animationOne();
+        setTimeout(function() { animationTwo() }, 2000);
+        setTimeout(function() { animationThree() }, 4000);
+    }
 });
