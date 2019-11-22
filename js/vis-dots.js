b8232d71b3bde2b52533d109289322c7d4ee7b39
Jessica Bishai <jess.bishai@gmail.com>
2019-11-21 22:59:17 -0500

    working on dot visualization

--- js/vis-dots.js
+++ js/vis-dots.js
@@ -20,8 +20,30 @@ d3.csv("data/allShootings.csv", function(data) {
     let dots = svgD.selectAll('.dot')
         .data(data);
     dots.enter().append('circle')
-        // .attr('class', function(d){
-        //     console.log(d)
-        //     return `customCircle ${d.age} ${d.killedBy}`
-        // })
+        .attr('class', function(d){
+            return `customCircle ${d.age} ${d.Type} ${d.Gender}`
+        })
+        .attr('cx', function(d,i){
+            return width/50*(3.5+i%40)
+            // if(i % 40 === 0){
+            //     return width/10*3.5;
+            // } else if(i % 40 === 1) {
+            //     return width/10 * 4.5
+            // } else if(i % 40 === 2) {
+            //     return width/10 * 5.5
+            // } else if(i % 4 === 3) {
+            //     return width/10 * 6.5
+            // }
+        })
+        .attr('cy', function(d,i){
+            return (Math.trunc(i / 40)) * height/50 + marginTop;
+        })
+        .attr('r', width/300)
+        // .attr('fill', function(d,i){
+        //     if(d.age === 'child'){
+        //         return 'blue'
+        //     }else{
+        //         return 'red'
+        //     }
+        // });
 });
