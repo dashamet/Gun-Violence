263f4652395e2548f9ce66dc56c6385ab9b18eca
dashamet <metropolitansky@college.harvard.edu>
2019-11-24 22:08:16 -0500

    Reformatted website and edited line graph

---
+++ js/my.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+    $('#pagepiling').pagepiling({
+        sectionsColor: ['white', 'white', 'white', 'white', 'white', 'white']
+    })
+
+    // Number counter on slide 2
+    $('.value').each(function () {
+        var $this = $(this);
+        jQuery({Counter: 0}).animate({Counter: $this.text()}, {
+            duration: 23000,
+            easing: 'swing',
+            step: function() {
+                var num = Math.ceil(this.Counter).toString();
+                if(Number(num) > 999){
+                    while (/(\d+)(\d{3})/.test(num)) {
+                        num = num.replace(/(\d+)(\d{3})/, '$1' + ',' + '$2');
+                    }
+                }
+                $this.text(num);
+            }
+        });
+    });
+
+    // Make one line appear at a time on slide 2
+    var lines = $('p').text().split("\n");
+    var counter = 26000
+    var timer,
+        displayLine = function(){
+            var nextLine = lines.shift();
+            if(nextLine){
+                var newLine = $('<li class="line">' + nextLine + '</li>');
+                $('#result').append(newLine);
+                newLine.animate({ 'margin-left':0 }, 300);
+                counter /= 7
+                timer = setTimeout(displayLine,counter);
+            }
+        }
+    timer = setTimeout(displayLine,counter);
+
+    //Typewriter effect for title
+    var str = "<p>Underage and Under Fire:<p style='color:white; font-size: 80px;'>The growing toll of guns on America's youth</p></p>",
+        i = 0,
+        isTag,
+        text;
+    (function type() {
+        text = str.slice(0, ++i);
+        if (text === str) return;
+
+        document.getElementById('typewriter').innerHTML = text;
+
+        var char = text.slice(-1);
+        if( char === '<' ) isTag = true;
+        if( char === '>' ) isTag = false;
+
+        if (isTag) return type();
+        setTimeout(type, 80);
+    }());
+
+});
+
+
--- js/vis-line.js
+++ js/vis-line.js
@@ -1,6 +1,6 @@
 // Creating variables for the SVG dimensions
-var margin = {top: 30, right: 180, bottom: 30, left: 80},
-    width = 1200 - margin.left - margin.right,
+var margin = {top: 30, right: 220, bottom: 10, left: 60},
+    width = 1100 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 // Setting the ranges
@@ -55,6 +55,7 @@ var valueline5 = d3.line()
     .x(d => x(d.year))
     .y(d => y(d.intentional_injuries))
 
+
 // appends the svg object to the body of the page
 // appends a 'group' element to 'svg'
 // moves the 'group' element to the top left margin
@@ -64,6 +65,54 @@ var svg = d3.select("#line-graph").append("svg")
     .append("g")
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
+
+function onMouseOver(){
+    var current_id = d3.select(this).attr("id")
+    var current_class = d3.select(this).attr("class")
+    d3.selectAll("#" + current_id)
+        .style("stroke-width","5px")
+        .style("font-size", "20px")
+        .style("stroke",  function(){
+            if(d3.select(this).attr("class") !== "text"){
+                return "red"
+            }
+        })
+        .style("fill", function(){
+            if(d3.select(this).attr("class") === "circle"){
+                return "red"
+            }
+            else if(d3.select(this).attr("class") === "line"){
+                return "white"
+            }
+        })
+    d3.selectAll(".line,.circle,.text")
+        .style("opacity", function() {
+            return (d3.select(this).attr('id') === current_id) ? 1.0 : 0.2;
+        })
+}
+
+function onMouseOut(){
+    d3.selectAll(".line,.circle,.text")
+        .transition()
+        .duration(500)
+        .style("stroke-width", "3px")
+        .style("font-size", "16px")
+        .style("opacity", 1.0)
+        .style("stroke", function(){
+            if(d3.select(this).attr("class") !== "text"){
+                return "black"
+            }
+        })
+        .style("fill", function(){
+            if(d3.select(this).attr("class") === "line"){
+                return "white"
+            }
+            else if(d3.select(this).attr("class") === "circle"){
+                return "black"
+            }
+        })
+}
+
 
 // Get the data
 d3.csv("data/aggregated-deaths-injuries.csv", function(data) {
@@ -102,58 +151,83 @@ d3.csv("data/aggregated-deaths-injuries.csv", function(data) {
     svg.append("path")
         .data([data])
         .attr("class", "line")
-        .style("stroke", "#4daf4a")
+        .attr('id', 'line1')
+        .style("stroke", "black")
         .style("fill","none")
         .attr("stroke-width", 2)
-        .attr("d", valueline);
+        .attr("d", valueline)
+        .on("mouseover", onMouseOver)
+        .on("mouseout", onMouseOut)
 
     // Add the valueline2 path.
     svg.append("path")
         .data([data])
         .attr("class", "line")
-        .style("stroke", "#e41a1c")
+        .attr("id", "line2")
+        .style("stroke", "black")
         .style("fill","none")
         .attr("stroke-width", 1.5)
         .attr("d", valueline2)
+        .on("mouseover", onMouseOver)
+        .on("mouseout", onMouseOut)
 
     // Add the valueline3 path.
     svg.append("path")
         .data([data])
         .attr("class", "line")
-        .style("stroke", "#377eb8")
+        .attr("id", "line3")
+        .style("stroke", "black")
         .style("fill","none")
         .attr("stroke-width", 1.5)
-        .attr("d", valueline3);
+        .attr("d", valueline3)
+        .on("mouseover", onMouseOver)
+        .on("mouseout", onMouseOut)
 
     // Add the valueline4 paths.
     svg.append("path")
         .datum(data.filter(valueline4.defined()))
-        .attr("stroke", "#ff7f00")
-        .style("stroke-dasharray", ("3, 3"))
+        .attr("class", "line")
+        .attr("id", "line4")
+        .style("stroke", "black")
+        .style("stroke-dasharray", ("2, 3"))
         .style("fill","none")
-        .attr("d", valueline4);
+        .attr("d", valueline4)
+        .on("mouseover", onMouseOver)
+        .on("mouseout", onMouseOut)
 
     svg.append("path")
         .datum(data)
-        .attr("stroke", "#ff7f00")
+        .style("stroke", "black")
+        .attr("class", "line")
+        .attr("id", "line4")
         .style("fill","none")
         .attr("stroke-width", 1.5)
-        .attr("d", valueline4);
+        .attr("d", valueline4)
+        .on("mouseover", onMouseOver)
+        .on("mouseout", onMouseOut)
 
     // Add the valueline5 paths.
     svg.append("path")
         .datum(data.filter(valueline5.defined()))
-        .attr("stroke", "#984ea3")
-        .style("stroke-dasharray", ("3, 3"))
+        .attr("class", "line")
+        .attr("id", "line5")
+        .style("stroke", "black")
+        .style("stroke-dasharray", ("2, 3"))
         .style("fill","none")
-        .attr("d", valueline5);
+        .attr("d", valueline5)
+        .on("mouseover", onMouseOver)
+        .on("mouseout", onMouseOut)
 
     svg.append("path")
         .datum(data)
-        .attr("stroke", "#984ea3")
+        .attr("class", "line")
+        .attr("id", "line5")
+        .style("stroke", "black")
         .style("fill","none")
         .attr("stroke-width", 1.5)
-        .attr("d", valueline5);
+        .attr("d", valueline5)
+        .on("mouseover", onMouseOver)
+        .on("mouseout", onMouseOut)
 
     var records = svg.selectAll(".records")
         .data(mod_data)
@@ -177,16 +251,29 @@ d3.csv("data/aggregated-deaths-injuries.csv", function(data) {
         })
         .attr("cx", function(d){return x(d.year)})
         .attr("cy", function(d){return y(d.victims)})
-        .attr("fill", function(d){return color[d.type]})
+        .attr("class", "circle")
+        .attr('id', function(d){
+            if (d.type === "intentional_homicides") {
+                return "line1";
+            } else if(d.type === "intentional_suicides"){
+                return "line2";
+            } else if(d.type === "unintentional_deaths"){
+                return "line3";
+            } else if(d.type === "unintentional_injuries"){
+                return "line4";
+            } else if(d.type === "intentional_injuries"){
+                return "line5";
+            }
+        })
         .attr("stroke", "black")
         .on('mouseover', function (d, i) {
             d3.select(this).transition()
-                .duration('50')
+                .duration(50)
                 .attr('opacity', '.85')
                 .attr('r', 8);
             div.transition()
                 .duration(50)
-                .style("opacity", 1);
+                .style("opacity", 1.5);
             div.html(function () {
                 if (isNaN(d.victims)) {
                     return "<strong>" + d.year.getFullYear() + "</strong>" + " <br> " + "No data available"
@@ -197,10 +284,10 @@ d3.csv("data/aggregated-deaths-injuries.csv", function(data) {
                 .style("left", (d3.event.pageX + 10) + "px")
                 .style("top", (d3.event.pageY - 15) + "px");
         })
-        .on('mouseout', function (d, i) {
+        .on('mouseout', function (d) {
             d3.select(this).transition()
-                .duration('50')
-                .attr('opacity', '1');
+                .duration(50)
+                .attr('r', 4);
             div.transition()
                 .duration('50')
                 .style("opacity", 0);
@@ -209,64 +296,76 @@ d3.csv("data/aggregated-deaths-injuries.csv", function(data) {
     // Add the X Axis
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
+        .attr("class", "axis")
         .call(d3.axisBottom(x));
 
     // Add the Y Axis
     svg.append("g")
+        .attr("class", "axis")
         .call(d3.axisLeft(y));
 
     // Add the Y Axis label
     svg.append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left + 10)
+        .attr("y", 0 - margin.left + 16)
         .attr("x",0 - (height / 2))
         .attr("dy", "1em")
+        .style("font-size", "14px")
         .style("text-anchor", "middle")
+        .style("fill", "grey")
         .text("Number of victims");
 
     // Add a title
-    svg.append("text")
-        .attr("x", (width / 2))
-        .attr("y", 0 - (margin.top / 2) + 10)
-        .attr("text-anchor", "middle")
-        .style("font-size", "20px")
-        .text("Firearm Deaths and Injuries Broken Down by Cause");
+    //svg.append("text")
+    //.attr("x", (width / 2))
+    //.attr("y", 0 - (margin.top / 2) + 10)
+    //.attr("text-anchor", "middle")
+    //.style("font-size", "20px")
+    //.text("Firearm Deaths and Injuries Broken Down by Cause");
 
     svg.append("text")
-        .attr("transform", "translate("+(width+10)+","+y(data[0].intentional_suicides + 300)+")")
+        .attr("transform", "translate("+(width-108)+","+y(data[0].intentional_homicides)+")")
         .attr("dy", ".35em")
         .attr("text-anchor", "start")
-        .style("fill", "#e41a1c")
-        .text("Suicides");
+        .style("fill", "black")
+        .text("Homicides")
+        .attr("class", "text")
+        .attr("id", "line1");
 
     svg.append("text")
-        .attr("transform", "translate("+(width+10)+","+y(data[0].intentional_homicides)+")")
+        .attr("transform", "translate("+(width-108)+","+y(data[0].intentional_suicides + 300)+")")
         .attr("dy", ".35em")
         .attr("text-anchor", "start")
-        .style("fill", "#4daf4a")
-        .text("Homicides");
+        .style("fill", "black")
+        .text("Suicides")
+        .attr("class", "text")
+        .attr("id", "line2");
 
     svg.append("text")
-        .attr("transform", "translate("+(width-197)+","+y(data[0].unintentional_injuries - 700)+")")
+        .attr("transform", "translate("+(width-108)+","+y(data[0].unintentional_deaths)+")")
         .attr("dy", ".35em")
         .attr("text-anchor", "start")
-        .style("fill", "#ff7f00")
-        .text("Injuries from accidents");
+        .style("fill", "black")
+        .text("Deaths from accidents")
+        .attr("class", "text")
+        .attr("id", "line3");
 
     svg.append("text")
-        .attr("transform", "translate("+(width-95)+","+y(data[0].intentional_homicides + 5500)+")")
+        .attr("transform", "translate("+(width-289)+","+y(data[0].unintentional_injuries - 698)+")")
         .attr("dy", ".35em")
         .attr("text-anchor", "start")
-        .style("fill", "#984ea3")
-        .text("Injuries from violence");
+        .style("fill", "black")
+        .text("Injuries from accidents")
+        .attr("class", "text")
+        .attr("id", "line4");
 
     svg.append("text")
-        .attr("transform", "translate("+(width+10)+","+y(data[0].unintentional_deaths)+")")
+        .attr("transform", "translate("+(width-197)+","+y(data[0].intentional_injuries + 2400)+")")
         .attr("dy", ".35em")
         .attr("text-anchor", "start")
-        .style("fill", "#377eb8")
-        .text("Deaths from accidents");
-
-
+        .style("fill", "black")
+        .text("Injuries from violence")
+        .attr("class", "text")
+        .attr("id", "line5");
 
 });
