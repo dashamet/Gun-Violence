b9e82c07697b56798d79b1091e062bf8ca45b7d9
Jessica Bishai <jess.bishai@gmail.com>
2019-11-25 19:17:14 -0500

    updated data

--- js/vis-map2.js
+++ js/vis-map2.js
@@ -27,32 +27,11 @@ var data = d3.map();
 queue()
     .defer(d3.json, "data/us.topo.json")
     .defer(d3.json, "data/us-states.json")
-   // .defer(d3.csv, "data/master.data.states.2019.01.18.csv")
+    .defer(d3.csv, "data/master.data.states.2019.01.18.csv")
     .await(createVisualization);
 
 
-function createVisualization(error, data) {
-
-    var selectedValue = d3.select("#data-value").property("value");
-
-    //load csv state names to topojson use data
-    d3.csv("data/us-state-names.csv", function (csv) {
-        var us = topojson.feature(data, data.objects.state).features;
-
-        csv.forEach(function (d, i) {
-            us.forEach(function (e, j) {
-                if (d.id === e.id) {
-                    e.name = d.name
-                }
-            })
-        });
-
-        svgM2.selectAll("map2")
-            .data(us)
-            .enter().append("path")
-            .attr("d", path)
-            .attr("fill", "lightgrey");
-    });
+function createVisualization(error, data, unused, deathData) {
 
     //if add more data, take the dots between the name of the column of the data you are using
     d3.csv("data/master.data.states.2019.01.18.csv", function (error, csv) {
@@ -75,10 +54,61 @@ function createVisualization(error, data) {
                 d.Persons15to19years = +d.Persons15to19years;
                 d.TotalChildPop = +d.TotalChildPop;
                 d.TotalFirearmDeaths = +d.TotalFirearmDeaths;
-
             });
-        
+    });
 
+    var deathData2016 = deathData.filter(function(data){
+        return data.Year === "2016";
+    });
+
+    var deathRates = [];
+    deathData2016.forEach(function(item, index){
+        const deathRate = item['TotalFirearmDeaths']/item['Population'];
+        item['death_crude_rate'] = deathRate;
+        deathRates[index] = deathRate
+    });
+
+    var colorScale = d3.scaleQuantize()
+        .domain(d3.extent(deathRates))
+        .range(["#fee5d9", "#fcbba1", "#fc9272", "#fb6a4a", "#de2d26", "#a50f15"]);
+
+
+    var selectedValue = d3.select("#data-value").property("value");
+
+    //load csv state names to topojson use data
+    d3.csv("data/us-state-names.csv", function (csv) {
+        var us = topojson.feature(data, data.objects.state).features;
+
+        csv.forEach(function (d, i) {
+            us.forEach(function (e, j) {
+                if (d.id === e.id) {
+                    e.name = d.name
+                }
+            })
+        });
+
+        svgM2.selectAll("map2")
+            .data(us)
+            .enter().append("path")
+            .attr("d", path)
+            .attr("fill", function(d){
+                var stateName = d.name;
+                var index = 0;
+                for (i = 0; i < deathData2016.length; i++) {
+                    if (deathData2016[i]['State'] === stateName) {
+                        index = i;
+                    }
+                }
+                var val = deathData2016[index]['death_crude_rate'];
+                if (isNaN(val)) {
+                    return "Grey"
+                }
+                else {
+                    return colorScale(val);
+                }
+            })
+            .attr("stroke", 'grey')
+            .attr("stroke-width", 0.1);
     });
 
 }
