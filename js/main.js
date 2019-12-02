5438cfd9f98e8ee67224fc5d298e7de8530a2496
Jessica Bishai <jess.bishai@gmail.com>
2019-12-02 17:40:50 -0500

    classes for dot vis

--- js/main.js
+++ js/main.js
@@ -8,15 +8,14 @@ let myBrushVis;
 
 // key
 // 0 = all shooting data
-let promises = [
-    //d3.json("https://cdn.jsdelivr.net/npm/us-atlas@3/counties-albers-10m.json"),
-    d3.csv("data/allShootings.csv")
-];
 
-Promise.all(promises)
-    .then( function(data){ initMainPage(data) })
-    .catch( function (err){console.log(err)} );
+// load data
+queue()
+    .defer(d3.csv, "data/allShootings.csv")
+    .defer(d3.csv, "data/us-state-names.csv")
+    .await(initMainPage);
 
-function initMainPage(dataArray) {
-    myDotVis = new Vis('memorial-viz', dataArray[0]);
+function initMainPage(error, shootingData, stateNameData) {
+    console.log(shootingData);
+    myDotVis = new dotVis('memorial-viz', shootingData);
 }
